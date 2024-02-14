670138ebcaf88e553410d36be7dc053a3de7c8c1
EFakLloyd <elisabeth.fak.lloyd@gmail.com>
2024-02-14 13:07:10 +0100

    Varelse med styrkor

--- SalesAdventure3000/Models/Models/Creature.cs
+++ SalesAdventure3000/Models/Models/Creature.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace Models.Models
 {
+    
+
+    
     public class Creature : Entity
     {
-        public Creature(char Character) : base(Character){ 
-        
+        protected int Strenght { get; set; }
+        protected int Vitality { get; set; }
+        protected int Patience {  get; set; }
+        protected int Charisma {  get; set; }
+        protected int Speed;
+        protected int Wisdom;
+        protected int Luck;
+
+        public Creature(int ID, string Name, int[] Coordinates, char Character) : base(ID, Name, Character)
+        {
+            this.Strenght = 9;
+            this.Vitality = 7;
+            this.Patience = 2;
+            this.Charisma = 1;
+            this.Speed = 8;
+            this.Wisdom = 4;
+            this.Luck = 1;
         }
 
 
